617f81c396854456a15334c61e7a541402801fab
nourar6 <nar6@hw.ac.uk>
2015-02-08 16:28:45 +0000

    Set up places_visited class
    Set up Text file which is being set into the places_visited class for
    the specific year.

---
+++ src/Taxi_Packages/Places_Visited.java
@@ -0,0 +1,20 @@
+package Taxi_Packages;
+
+import java.util.List;
+import java.util.HashMap;
+
+public class Places_Visited {
+
+    HashMap<Integer, List> places_by_year = new HashMap<Integer, List>();
+
+    public void set_places_visited(List<String> places_visited, int year) {
+
+        places_by_year.put(year, places_visited);
+    }
+
+    public List<String> get_places_visited(int year) {
+
+        return places_by_year.get(year);
+
+    }
+}
--- src/Taxi_Service.java
+++ src/Taxi_Service.java
@@ -1,9 +1,17 @@
+import Taxi_Packages.Places_Visited;
 import Taxi_Packages.Taxis;
 
+import javax.swing.table.TableCellEditor;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.json.*;
 
 public class Taxi_Service {
 
@@ -12,24 +20,42 @@ public class Taxi_Service {
 
     public static void main(String[] args) throws FileNotFoundException,IOException {
 
+        TreeMap Taxis_Map = new TreeMap();
+        Places_Visited places =  new Places_Visited();
+
+        // CSV READER Setting
+        // gs
         String DELIMITER = ",";
-        int FIELD_TO_SUM = 2;
-        int sum = 0;
-        int lineCount = 0;
-        System.out.println(System.getProperty("user.home").concat("\\Desktop\\Taxi-Service\\src\\drivers.csv"));
+
         BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.home").concat("\\Desktop\\Taxi-Service\\src\\drivers.csv")));
         String line = null;
+        //CSV Loop
         while ((line = reader.readLine()) != null) {
-            String[] split = line.split(DELIMITER);
 
-            lineCount++;
+            String[] split = line.split(DELIMITER);
             System.out.println(split[0].concat(", " + split[1]));
             Taxis taxi =  new Taxis();
             taxi.setDriver(split[0], split[1]);
+            Taxis_Map.put(taxi.getRegistration(), taxi);
 
-            System.out.println(taxi.getDriver());
-            System.out.println(taxi.getRegistration());
         }
+        reader = new BufferedReader(new FileReader(System.getProperty("user.home").concat("\\Desktop\\Taxi-Service\\src\\destinations_2014.csv")));
+        // specify year for places_visited
+        int year = 2014;
+        // List of places_visited set as empty
+        List<String> places_visited = new ArrayList<String>();
+        // While loop to loop over each line of places
+        while ((line = reader.readLine()) != null) {
+            //add the places to the list
+            places_visited.add(line);
+        }
+        //once list is finished add the list with the year to our places_visited class
+        places.set_places_visited(places_visited, year);
+
+        System.out.println(places.get_places_visited(2014));
+
+
+
     }
 
 
